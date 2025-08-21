f2c759dca7338c16c4e2e4bbd14a3797da22e7e1
Ganavinr <160474394+Ganavinr@users.noreply.github.com>
2025-08-21 21:46:59 +0530

    Update EmployeeDemo.java

--- tnsif-master/Corejava/src/com/ait/day8/static1/EmployeeDemo.java
+++ tnsif-master/Corejava/src/com/ait/day8/static1/EmployeeDemo.java
@@ -3,7 +3,7 @@ package com.ait.day8.static1;
 public class EmployeeDemo {
 
 	public static void main(String[] args) {
-		Employee e= new Employee("Bharath",20);
+		Employee e= new Employee("Ganavi",20);
 		System.out.println(e);
 	}
 }

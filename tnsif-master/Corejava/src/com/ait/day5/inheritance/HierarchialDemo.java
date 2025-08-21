1d6bfe4dba578c75c1d7d209a2e7e0d7c3631fa5
Ganavinr <160474394+Ganavinr@users.noreply.github.com>
2025-08-21 21:40:52 +0530

    Update HierarchialDemo.java

--- tnsif-master/Corejava/src/com/ait/day5/inheritance/HierarchialDemo.java
+++ tnsif-master/Corejava/src/com/ait/day5/inheritance/HierarchialDemo.java
@@ -4,9 +4,9 @@ public class HierarchialDemo {
 
 	public static void main(String[] args) {
 		Person p;
-		p=new Student1("Ashansh","Bangalore","6th sem",97.5f);
+		p=new Student1("Ganavi","Bangalore","6th sem",97.5f);
 		System.out.println(p);
-		p=new Employee("ISE","Bangalore",020,20000,"Ashansh");
+		p=new Employee("ISE","Bangalore",020,20000,"Ganavi");
 		System.out.println(p);
 
 	}
